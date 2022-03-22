bcded47d2c6a24f7d7ed5f52cc0abdf7aebe0ab1
Peter Marshall <peterdmarshall99@gmail.com>
2022-03-22 16:43:21 -0400

    Duplicate data for testing

--- backend/app/redis/buffer.js
+++ backend/app/redis/buffer.js
@@ -52,12 +52,14 @@ const flushBuffer = async (userId, sessionId) => {
 
   let transposedData = [[], [], [], [], [], []];
   formattedData.forEach(el => {
-    transposedData[0].push(el[0])
-    transposedData[1].push(el[1])
-    transposedData[2].push(el[2])
-    transposedData[3].push(el[3])
-    transposedData[4].push(el[4])
-    transposedData[5].push(el[5])
+    for (let i = 0; i < 4; i++) {
+      transposedData[0].push(el[0])
+      transposedData[1].push(el[1])
+      transposedData[2].push(el[2])
+      transposedData[3].push(el[3])
+      transposedData[4].push(el[4])
+      transposedData[5].push(el[5])
+    }
   })
 
   return {
